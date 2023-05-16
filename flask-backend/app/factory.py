85fcaaf5b930b86664402bc376d419d23fff0dbe
KOYAMANI <ykkg3ym1114@gmail.com>
2023-05-16 13:52:17 +0200

    update docker file docker compose

--- flask-backend/app/factory.py
+++ flask-backend/app/factory.py
@@ -8,6 +8,11 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+
+    print("Using environment: ")
+    env = os.getenv("FLASK_ENV")
+    print(env)
+
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
