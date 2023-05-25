0378eca7340be160441279b2b6e8a3fa13ab1920
KOYAMANI <ykkg3ym1114@gmail.com>
2023-05-25 09:56:54 +0200

    fix env switch init.py

--- flask-backend/app/__init__.py
+++ flask-backend/app/__init__.py
@@ -14,27 +14,21 @@ def create_app():
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
 
-    debug = os.getenv("FLASK_DEBUG", "false").lower() == "true"
-    env = "production" if debug == False else "development"
+    debug = os.getenv("FLASK_DEBUG", "false").lower() in ["true", "1"]
 
     secrets_json = os.getenv("SECRETS_JSON")
     secrets = json.loads(secrets_json)
 
-    if env == "development":
+    if debug == True:
         app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI_DEV")
-        pass
-    elif env == "production":
+        logger.info(f"Application starting on Development environment")
+    else:
         if not secrets_json:
             raise ValueError("SECRETS_JSON is not set")
-
         app.config["SQLALCHEMY_DATABASE_URI"] = secrets["SQLALCHEMY_DATABASE_URI_PROD"]
         app.config["X_MAL_CLIENT_ID"] = secrets["X_MAL_CLIENT_ID"]
         app.config["MAL_API_URL"] = secrets["MAL_API_URL"]
-        print(secrets["SQLALCHEMY_DATABASE_URI_PROD"])
-    else:
-        raise ValueError(f"Invalid FLASK_ENV: {env}")
-
-    logger.info(f"Application starting on {env} environment")
+        logger.info(f"Application starting on Production environment")
 
     app.config["CORS_HEADERS"] = "Content-Type"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
