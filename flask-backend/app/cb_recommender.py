ceaf7c699cdf01bc7373dc5f928f5f8717b1b44f
KOYAMANI <ykkg3ym1114@gmail.com>
2023-05-27 11:45:45 +0200

    fix circular import

--- flask-backend/app/cb_recommender.py
+++ flask-backend/app/cb_recommender.py
@@ -3,7 +3,7 @@ import numpy as np
 import json
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from .dataset_handler import DatasetHandler
+from .anime_data_handler import AnimeDataHandler
 
 
 class CBRecommender:
@@ -16,12 +16,12 @@ class CBRecommender:
         return cls._instance
 
     def __init__(self):
-        self.dataset_handler = DatasetHandler()
-        self.ANIME_DATASET = self.dataset_handler.load_anime_data()
+        self.anime_data_handler = AnimeDataHandler()
+        self.ANIME_DATASET = self.anime_data_handler.load_anime_data()
         self.SIMILARITY = self.calc_similarity()
 
     def calc_similarity(self):
-        # ANIME_DATASET = self.dataset_handler.load_anime_data()
+        # ANIME_DATASET = self.anime_data_handler.load_anime_data()
         vectorizer = CountVectorizer()
         genre_matrix = vectorizer.fit_transform(self.ANIME_DATASET["Genres"])
         genre_similarity = cosine_similarity(genre_matrix, genre_matrix)
@@ -30,8 +30,10 @@ class CBRecommender:
     def get_rec(self, anime_title, k=10):
         index = self.ANIME_DATASET[self.ANIME_DATASET["Name"] == anime_title].index[0]
         scores = self.SIMILARITY[index]
-        top_k_indices = np.argpartition(scores, -k)[-k:]
-        top_k_indices_sorted = top_k_indices[np.argsort(scores[top_k_indices])][::-1]
-        res = self.ANIME_DATASET["Name"].iloc[top_k_indices_sorted].tolist()
 
-        return '{"status":"SUCCESS", "data":' + json.dumps(res) + "}"
+        top_k_indices = np.argpartition(scores, -k)[-k:]
+        top_k_indices_sorted = top_k_indices[np.argsort(scores[top_k_indices])][::-2]
+
+        ids = self.ANIME_DATASET["MAL_ID"].iloc[top_k_indices_sorted].tolist()
+
+        return '{"status":"SUCCESS", "id":' + json.dumps(ids) + "}"
