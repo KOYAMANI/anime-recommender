ced1dcae3b10cab0c3240db4bb6236198aabdcae
KOYAMANI <ykkg3ym1114@gmail.com>
2023-06-05 17:54:19 +0200

    update user page to show search history

--- frontend/src/Pages/User.tsx
+++ frontend/src/Pages/User.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import APIService from '../Components/APIService'
+import ResultContainer from '../Components/pageSpecific/main/ResultContainer'
 
 const User: React.FC = () => {
     const [username, setUsername] = useState('')
     const [userId, setUserId] = useState('')
+    const [animes, setAnimes] = useState([])
     const navigate = useNavigate()
     const apiService = new APIService()
 
@@ -20,7 +22,7 @@ const User: React.FC = () => {
         if (userId) {
             setUserId(userId)
             apiService.getUserHistory(userId).then((res) => {
-            console.log(res)
+                setAnimes(res)
             })
         } else console.log('no userId')
 
@@ -29,9 +31,10 @@ const User: React.FC = () => {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white p-4">
             <h1 className="text-3xl mb-4">
-                Synced with your MyAnimeList account
+                Welcome back {username}
             </h1>
-            <p>Welcome back {username}</p>
+            <p>Your last seach</p>
+            <ResultContainer animes={animes} />
         </div>
     )
 }
